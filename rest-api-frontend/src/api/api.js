ef69e1cd4697b3e00778a121f6c81901ad12c3e9
rameshworchaudhary <194390492+rameshworchaudhary@users.noreply.github.com>
2026-08-20 15:01:14 +0530

    Connect With Backend

--- rest-api-frontend/src/api/api.js
+++ rest-api-frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://full-stack-0yf4.onrender.com";
 
 async function request(endpoint, options = {}) {
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
